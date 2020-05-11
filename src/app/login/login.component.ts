43499bced935bcb56bc07a5e92affd12b1b98b8e
krishna sandeep <vanamasandeep2019@gmail.com>
2020-05-11 10:19:19 +0530

    Print username values in the console

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  username='sandeep89';
+  password=''
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  handleLogin(){
+    console.log(this.username);
+    console.log(this.password);
   }
 
 }
